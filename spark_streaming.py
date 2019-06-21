121da35e6ecc8dc905859a12447fc2bb580edd15
Dung Cao <ctdung258@gmail.com>
2019-06-21 16:49:55 +0700

    update spark streaming

--- spark_streaming.py
+++ spark_streaming.py
@@ -1,9 +1,10 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
-# Create a local StreamingContext with two working threads and a batch interval of 2 seconds
+# Create a local StreamingContext with two working threads and a batch interval of 60 seconds
 sc = SparkContext("local[2]", "NetworkWordCount")
-ssc = StreamingContext(sc, 2)
+ssc = StreamingContext(sc, 60)
+sc.setLogLevel('ERROR')
 
 # Create a DStream
 lines = ssc.socketTextStream("localhost", 9999)
