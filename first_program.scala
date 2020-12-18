e58aad69db32b608a2f5a3769630d4f8d511ae8f
Dung Cao <ctdung258@gmail.com>
2020-12-18 09:11:26 +0700

    Delete first_program.scala

--- first_program.scala
+++
@@ -1,9 +0,0 @@
-class Customers(name: String, debts: List[Int], credit: Int) {
-	def totalDebt: Int = {
-		debts.foldLeft()((x,y) => x+y)
-	}
-}
-
-var debts = List(100,200,300)
-var cus = new Customers("Dung Cao", debts, 1500)
-println(cus.totalDebt)
