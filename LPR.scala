ab5b8f360597dea1fe8f8461b1eb0017f1366e59
Dung Cao <ctdung258@gmail.com>
2021-09-07 23:19:32 +0700

    Update LPR.scala

--- LPR.scala
+++ LPR.scala
@@ -2,7 +2,9 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 
-//./bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,org.elasticsearch:elasticsearch-spark-20_2.12:7.13.1 /Users/dungcao/IdeaProjects/Optymizing/out/artifacts/Optymizing_jar/Optymizing.jar  13.212.213.41:9092 test_topic localhost 9200
+//./bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,org.elasticsearch:elasticsearch-spark-20_2.12:7.13.1 
+// xxx.jar  kafka_host:9092 kafka_topic 
+// es_host 9200
 
 object LPR {
   val spark: SparkSession = SparkSession
@@ -15,56 +17,7 @@ object LPR {
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-//    val schema = new StructType()
-//      .add("messageid", StringType)
-//      .add("mdsversion", StringType)
-//      .add("@timestamp", StringType)
-//      .add("place", StringType)
-//      .add("sensor", new StructType()
-//        .add("id", StringType)
-//        .add("type", StringType)
-//        .add("description", StringType)
-//        .add("location", new StructType()
-//          .add("lat", DoubleType)
-//          .add("lon", DoubleType)
-//          .add("alt", DoubleType))
-//        .add("coordinate", new StructType()
-//          .add("x", DoubleType)
-//          .add("y", DoubleType)
-//          .add("z", DoubleType)))
-//      .add("analyticsModule", StringType)
-//      .add("object", new StructType()
-//        .add("id", StringType)
-//        .add("speed", DoubleType)
-//        .add("direction", DoubleType)
-//        .add("orientation", DoubleType)
-//        .add("vehicle", new StructType()
-//          .add("type", StringType)
-//          .add("make", StringType)
-//          .add("model", StringType)
-//          .add("color", StringType)
-//          .add("licenseState", StringType)
-//          .add("license", StringType)
-//          .add("confidence", DoubleType))
-//        .add("bbox", new StructType()
-  //        .add("topleftx", DoubleType)
-  //        .add("toplefty", DoubleType)
-  //        .add("bottomrightx", DoubleType)
-  //        .add("bottomrighty", DoubleType)))
-//      .add("location", new StructType()
-//        .add("lat", DoubleType)
-//        .add("lon", DoubleType)
-//        .add("alt", DoubleType))
-//      .add("coordinate", new StructType()
-//        .add("x", DoubleType)
-//        .add("y", DoubleType)
-//        .add("z", DoubleType))
-//      .add("event", new StructType()
-//        .add("id", StringType)
-//        .add("type", StringType))
-//      .add("videoPath", StringType)
-
-    if(args.length < 4){
+    if(args.length < 2){
       println()
       println("=================== MISSING ARG LIST ================================")
       println("ARGS: <kafka-conn-str> <topic> <es_host> <es_port>")
@@ -74,13 +27,13 @@ object LPR {
 
     val kafka_conn_str = args(0)
     val kafka_topic = args(1)
-    val es_host = args(2)
-    val es_port = args(3)
+//    val es_host = args(2)
+//    val es_port = args(3)
 
     val df = spark.readStream
               .format("kafka")
-              .option("kafka.bootstrap.servers", kafka_conn_str)  //13.212.213.41:9092")
-              .option("subscribe", kafka_topic)   //"test_topic")
+              .option("kafka.bootstrap.servers", kafka_conn_str) 
+              .option("subscribe", kafka_topic)   
 //              .option("startingOffsets", "earliest")
               .load()
 
@@ -102,11 +55,9 @@ object LPR {
         get_json_object(col("value"), "$.event.type").alias("event"),
         get_json_object(col("value"), "$.videoPath").alias("imagepath"))
       .filter("messageid is not null")
-//      .withWatermark("timestamp", "10 minutes")
       .groupBy(
-//        window($"timestamp", "10 minutes", "5 minutes"),
         $"license").agg(
-//              first("messageid").as('msgid'),
+              first("messageid").as('msgid'),
               first("timestamp").as("timestamp"),
               first("place").as("place"),
               first("sensor").as("sensor"),
@@ -120,21 +71,21 @@ object LPR {
               first("imagepath").as("filename"))
 
 //    ========= TO ELASTICSEARCH ========
-    data_to_es.writeStream
-      .foreachBatch{(batchDF: DataFrame, batchId: Long) =>
-      batchDF.write.format("org.elasticsearch.spark.sql")
-        .option(ConfigurationOptions.ES_NODES, es_host)
-        .option(ConfigurationOptions.ES_PORT, es_port)
-        .mode("append")
-        .save("optymizing/docs")
-    }.outputMode("update")
-      .start().awaitTermination()
+//    data_to_es.writeStream
+//      .foreachBatch{(batchDF: DataFrame, batchId: Long) =>
+//      batchDF.write.format("org.elasticsearch.spark.sql")
+//        .option(ConfigurationOptions.ES_NODES, es_host)
+//        .option(ConfigurationOptions.ES_PORT, es_port)
+//        .mode("append")
+//        .save("optymizing/docs")
+//    }.outputMode("update")
+//      .start().awaitTermination()
 
 //    ========== TO CONSOLE ============
-//    data_to_es.writeStream
-//        .format("console")
-//        .outputMode("update")
-//        .start().awaitTermination()
+    data_to_es.writeStream
+        .format("console")
+        .outputMode("update")
+        .start().awaitTermination()
 
 //    =========== TO HDFS ==========
 //    data.writeStream
