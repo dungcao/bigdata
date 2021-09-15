95276ab4e0497400debf78b66d0affadb5d921d5
Dung Cao <ctdung258@gmail.com>
2021-09-15 16:33:18 +0700

    Update spark_weblog_mining.py

--- spark_weblog_mining.py
+++ spark_weblog_mining.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 sc = SparkContext("local", "weblog mining app")
-rdd1 = sc.textFile('hdfs://localhost:9000/wedlog.csv')
+rdd1 = sc.textFile('hdfs://localhost:9000/weblog.csv')
 
 counts = rdd1.filter(lambda s: s.split(',')[2].startswith("GET")).map(lambda s: (s.split(',')[1].split(':')[0][1:],1))
 
